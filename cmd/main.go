2755cfdee0cc7870edec92eecb98f630adb0d787
siq8 <aladakaverka@gmail.com>
2025-07-20 00:32:23 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"practical/internal/api"
 )
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		c.File("index.html")
+	})
 	r.POST("/calculate", api.Processor)
 	r.Run()
 }
